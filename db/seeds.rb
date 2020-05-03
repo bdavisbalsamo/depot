08baa6d26eb414eedeb2e8ab9e5aceb3c814ddd6
Brandy Davis-Balsamo <brandy@carbonfive.com>
2020-05-03 11:42:05 -0400

    layout changes for input type

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,6 @@ Product.create(title: 'Programming Ruby 1.9 & 2.0',
         out there. If you need to get working programs delivered fast,
         you should add Ruby to your toolbox.
       </p>},
-  image_url: 'ruby.jpg',
+  image: 'ruby.jpg',
   price: 49.95)
   #
-  
